ab79bce6ade2585acfc57f77f7f786ed5b72906e
tabbot02 <tabbot02@GWHXX582WIN021A>
2018-02-13 10:45:02 +0000

    Initial database created

--- GITTest/Form1.Designer.cs
+++ GITTest/Form1.Designer.cs
@@ -50,6 +50,7 @@
             this.label2.Size = new System.Drawing.Size(10, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = ".";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
             // 
--- GITTest/Form1.cs
+++ GITTest/Form1.cs
@@ -21,5 +21,10 @@ namespace GITTest
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            //comment here
+        }
     }
 }
